2df8168806b48e4a4bb2ef38daabae1d621ab00b
Aman Shrestha <amanshrestha147181@gmail.com>
2024-02-24 14:03:32 +0545

    added about us component

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,18 +1,54 @@
-import React from 'react'
+import React from "react";
 
 const AboutUs = () => {
   return (
-    <div className='w-full bg-[#f3f3ea] px-10'>
-      <div className='grid grid-cols-2'>
-        <div className='col-span-1'>
-          
+    <div className="w-full pt-16 px-5 sm:px-12 md:px-16 lg:px-20">
+      <div className="grid grid-cols-1 gap-12 xl:gap-20  xl:grid-cols-2">
+        <div className="col-span-1 flex flex-col space-y-5">
+          <h1 className="text-4xl font-medium font-volkhov text-[#254D4D]">About Us</h1>
+          <h1 className="text-xl text-[#254D4D]  font-roboto">
+            Founded in 2003, Urban Dwellings has been a labor of love, born out
+            of a passion for design and a desire to redefine the way people
+            experience their homes.{" "}
+          </h1>
+          <h1 className="text-3xl pt-5 font-volkhov font-medium text-[#254D4D]">What Sets Us Apart</h1>
+          <div className="grid sm:grid-cols-2 gap-5 grid-cols-1">
+            <div className="col-span-1">
+              <h1 className="text-xl text-[#254D4D] font-bold font-roboto pb-2">Quality Craftsmanship</h1>
+              <h1 className="text-lg text-[#254D4D] font-roboto">
+                We source materials meticulously to ensure durability and
+                longevity.
+              </h1>
+            </div>
+            <div className="col-span-1">
+            <h1 className="text-xl text-[#254D4D] font-bold font-roboto pb-2">Unique Designs</h1>
+            <h1 className="text-lg text-[#254D4D] font-roboto">
+                Discover unique designs that capture the essence of different
+                styles
+              </h1>
+            </div>
+            <div className="col-span-1">
+            <h1 className="text-xl text-[#254D4D] font-bold font-roboto pb-2">Customer-Centric Approach</h1>
+            <h1 className="text-lg text-[#254D4D] font-roboto">
+                Our team is dedicated to ensuring you have a seamless enjoyable
+                shopping experience
+              </h1>
+            </div>
+            <div className="col-span-1">
+            <h1 className="text-xl text-[#254D4D] font-bold font-roboto pb-2">Sustainable Practices</h1>
+            <h1 className="text-lg text-[#254D4D] font-roboto">
+                Many of our products are crafted using sustainable and
+                eco-friendly manufacturing processes.
+              </h1>
+            </div>
+          </div>
         </div>
-        <div className='col-span-1'>
-          
+        <div className="col-span-1 hidden xl:block">
+          <img src="/about.png" className="w-full max-h-[460px]" />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutUs
+export default AboutUs;
