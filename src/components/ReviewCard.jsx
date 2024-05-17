ddba86071baeac283179455427f2ac926cd861dd
Aman Shrestha <amanshrestha147181@gmail.com>
2024-05-17 15:42:52 +0545

    changed testimonials

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -1,28 +1,47 @@
-import star from "/star.svg";
-const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
-    return (
-      <div className='flex justify-center items-center flex-col'>
-        <img
+import { Image } from "@nextui-org/react"
+import { GrLinkPrevious } from "react-icons/gr";
+import { GrLinkNext } from "react-icons/gr";
+const ReviewCard = ({ imgURL, customerName,  feedback, activeSlide, setActiveSlide }) => {
+  return (
+    <div className="grid grid-cols-1  lg:grid-cols-2 gap-8 gap-y-5  text-center w-full h-full justify-center items-center ">
+      <div className="col-span-1 flex items-center justify-center ">
+        <Image
           src={imgURL}
-          alt='customer'
-          className='rounded-full object-cover w-[120px] h-[120px]'
+          alt="customer"
+          width={300}
+          height={300}
+          
         />
-        <p className='mt-6 max-w-sm text-center info-text'>{feedback}</p>
-        <div className='mt-3 flex justify-center items-center gap-2.5'>
-          <img
-            src={star}
-            width={24}
-            height={24}
-            alt='rating star'
-            className='object-contain m-0'
-          />
-          <p className='text-xl font-volkhov text-slate-800'>({rating})</p>
-        </div>
-        <h3 className='mt-1  text-3xl text-center text-[#1a2e2e] font-bold'>
+      </div>
+      <div className="col-span-1 relative text-center items-center justify-center lg:items-start flex flex-col h-full space-y-5 sm:space-y-10">
+        <p className="mt-6  text-center lg:text-left text-sm sm:text-lg">{feedback}</p>
+
+        <h3 className="mt-1  text-3xl sm:text-4xl text-center lg:text-left text-[#1a2e2e] font-bold">
           {customerName}
         </h3>
+        <div className=" flex w-full lg:absolute sm:pr-10 sm:bottom-0  space-x-2 sm:space-x-4 sm:justify-end justify-center items-center">
+          <button
+            className="p-3 bg-slate-200 sm:hover:bg-slate-300 rounded-full"
+            onClick={() => {
+              if (activeSlide == 1) setActiveSlide(4);
+              else setActiveSlide(activeSlide - 1);
+            }}
+          >
+            <GrLinkPrevious />
+          </button>
+          <button
+            className="p-3 bg-slate-200 hover:bg-slate-300 rounded-full"
+            onClick={() => {
+              if (activeSlide == 4) setActiveSlide(1);
+              else setActiveSlide(activeSlide + 1);
+            }}
+          >
+            <GrLinkNext />
+          </button>
+        </div>
       </div>
-    );
-  };
-  
-  export default ReviewCard;
+    </div>
+  );
+};
+
+export default ReviewCard;
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,50 +1,47 @@
-import { Autoplay, Pagination, A11y } from "swiper/modules";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
-import "swiper/css/scrollbar";
-
+import { useState } from "react";
 import ReviewCard from "./ReviewCard";
 
 const Testimonial = () => {
+  const [activeSlide, setActiveSlide] = useState(1);
   const reviews = [
     {
+      id: 1,
       customerName: "Barry Allen",
       rating: 5.0,
       feedback:
         "Urban Dwellings exceeded my expectations! Their craftsmanship and attention to detail are top-notch. Highly recommended for quality and style.",
-      imgURL: "customer1.jpeg",
+      imgURL: "/customer1.jpeg",
     },
     {
+      id: 2,
       customerName: "Patty Spivot",
       rating: 4.8,
       feedback:
         "Thrilled with my Urban Dwellings purchase! Seamless blend of style and comfort. Outstanding customer service. I'll be back!",
-      imgURL: "customer2.jpeg",
+      imgURL: "/customer2.jpeg",
     },
-    
+
     {
+      id: 3,
       customerName: "Eobard Thawne",
       rating: 4.9,
       feedback:
         "Urban Dwellings met and exceeded my expectations! Top-notch quality with a personalized touch. Highly recommended!",
-      imgURL: "customer4.jpg",
+      imgURL: "/customer4.jpg",
     },
     {
+      id: 4,
       customerName: "Hunter Zolomon",
       rating: 4.7,
       feedback:
         "Impressive quality from Urban Dwellings! The virtual showroom made choosing easy. A new standard in furniture. Well done!",
-      imgURL: "customer3.jpg",
+      imgURL: "/customer3.jpg",
     },
   ];
 
   return (
-    <section className="px-5 py-10">
+    <section className="bg-slate-50 md:mx-20 mx-5 mb-10 py-10">
       <h3 className=" text-center text-[#254D4D] text-4xl font-bold">
         What Our
         <span className="text-[#C14E07]"> Customers </span>
@@ -55,42 +52,21 @@ const Testimonial = () => {
         exceptional experiences with us.
       </p>
 
-      <div className="mt-16 flex gap-14">
-        <Swiper
-          style={{
-            "--swiper-pagination-color": "#254D4D",
-          }}
-          slidesPerView={1}
-          spaceBetween={10}
-          modules={[Autoplay, Pagination, A11y]}
-          autoplay={{
-            delay: 2000,
-            disableOnInteraction: false,
-          }}
-          breakpoints={{
-            740: {
-              slidesPerView: 2,
-              spaceBetween: 20,
-            },
-            1224: {
-              slidesPerView: 3,
-              spaceBetween: 40,
-            },
-          }}
-          pagination={{ clickable: true }}
-        >
-          {reviews.map((review, index) => (
-            <SwiperSlide className="pb-20" key={index}>
-              <ReviewCard
-                key={index}
-                imgURL={review.imgURL}
-                customerName={review.customerName}
-                rating={review.rating}
-                feedback={review.feedback}
-              />
-            </SwiperSlide>
+      <div className="mt-16 px-10 flex relative min-h-80  items-center w-full justify-center ">
+        {reviews
+          .filter((slide) => slide.id === activeSlide)
+          .map((review, index) => (
+            <ReviewCard
+              key={index}
+              imgURL={review.imgURL}
+              customerName={review.customerName}
+              rating={review.rating}
+              feedback={review.feedback}
+              activeSlide={activeSlide}
+              setActiveSlide={setActiveSlide}
+            />
           ))}
-        </Swiper>
+        
       </div>
     </section>
   );
